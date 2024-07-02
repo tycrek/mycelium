e2d334b02157aad09dba0a75a347d0ab5367a8c1
Sylvie <sylvie@tycrek.com>
2024-07-02 16:24:32 -0600

    feat: wrote the thing

--- wemo.py
+++ wemo.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python
 
-import json
 import pywemo
+import schedule
+import socket
 from flask import Flask
+from flask_apscheduler import APScheduler
 
+# flask
 app = Flask(__name__)
+
+# scheduler
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+# wemo device
+wemo_url = pywemo.setup_url_for_address(open('wemo_ip', 'r').readlines()[0].strip())
+wemo_device = pywemo.discovery.device_from_description(wemo_url)
 
 @app.route('/toggle')
 def toggle():
-	url = pywemo.setup_url_for_address(open('wemo_ip', 'r').readlines()[0].strip())
-	device = pywemo.discovery.device_from_description(url)
-	device.toggle()
-	return json.dumps({'toggled': True})
+	wemo_device.toggle()
+	return ':3'
 
-@app.route('/')
-def index():
-	return 'hi'
+def netcheck():
+	try:
+		host = socket.gethostbyname('one.one.one.one')
+		sock = socket.create_connection((host, 80), 2)
+		sock.close()
+	except:
+		pass
+		print('network probably down')
+		toggle()
+		time.sleep(10)
+		toggle()
 
-app.run()
+scheduler.add_job(id = 'wemo', func = netcheck, trigger = 'interval', seconds = 5)
+app.run(host = '0.0.0.0')
